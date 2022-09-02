d15a1fb552856a26f77b7940dfb718cea696d188
vlad <vzhary@vepay.online>
2022-09-02 17:26:47 +0300

    new test file

--- conftest.py
+++ conftest.py
@@ -10,16 +10,17 @@ def pytest_addoption(parser):
 
 @pytest.fixture()
 def browser(request):
-    browser_language = request.config.getoption("language")
-    options = Options()
-    if browser_language == 'en':
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
-        browser = webdriver.Chrome(options=options)
-    elif browser_language == 'ru':
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'ru'})
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be en or ru")
+    # browser_language = request.config.getoption("language")
+    browser = webdriver.Chrome()
+    # options = Options()
+    # if browser_language == 'en':
+    #     options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
+    #     browser = webdriver.Chrome(options=options)
+    # elif browser_language == 'ru':
+    #     options.add_experimental_option('prefs', {'intl.accept_languages': 'ru'})
+    #     browser = webdriver.Chrome(options=options)
+    # else:
+    #     raise pytest.UsageError("--language should be en or ru")
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
--- pages/locators.py
+++ pages/locators.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_BUTTON = (By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
 
 
 class LoginPageLocators:
     LOGIN_FORM = (By.XPATH, '//form[@id="login_form"]')
-    REGISTER_FORM = (By.XPATH, '//form[@id="register_form"]'
-                     )
+    REGISTER_FORM = (By.XPATH, '//form[@id="register_form"]')
